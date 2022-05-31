386a3bc38d93dec159e2d5d67edf81f58051e8e7
chippy <chippyraj523538@gmail.com>
2022-05-30 19:01:14 -0500

    changes in app4

--- app4.py
+++ app4.py
@@ -1,1 +1,42 @@
-impor
+import random
+top_of_range = input("enter the number : ")
+
+if top_of_range.isalpha():
+    print("enter a number")
+    quit()
+
+top_of_range = int(top_of_range)
+
+if top_of_range <= 0:
+    print("enter a value greater than zero ")
+    quit()
+
+random_number = random.randint(0, top_of_range)
+guess =0
+
+while guess < 5:
+    user_guess = input("enter a guess number ")
+
+    if user_guess.isalpha():
+        print("enter a number")
+        continue
+
+    user_guess = int(user_guess)
+
+    if user_guess <= 0:
+        print("enter a value greater than zero ")
+        continue
+
+    guess = + 1
+
+    if user_guess == random_number:
+        print("you got it")
+        break
+
+    elif user_guess > random_number:
+        print("you are above the range")
+    else:
+        print("you are below the range ")
+
+print("you got your answer in " + str(guess) + "  guess")
+
